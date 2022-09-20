1647b51fc2eff44dd99544071cdf89aa485566e7
theantidoak <tcdoak@gmail.com>
2022-09-20 22:12:44 +0200

    Add isStorage function

--- src/localStorage.js
+++ src/localStorage.js
@@ -12,9 +12,15 @@ function populateStorage(callName, item) {
 }
 
 function renderStorageOnPage() {
-    if (localStorage.length == 0) return;
-    findProjectStorageAndRender();
-    findTodoStorageAndRender();
+    
+    if (storageAvailable('localStorage')) {
+        if (localStorage.length == 0) return;
+        findProjectStorageAndRender();
+        findTodoStorageAndRender();
+        }
+      else {
+        // Too bad, no localStorage for us
+      }
 }
 
 function findProjectStorageAndRender() {
@@ -150,5 +156,33 @@ function changeDataID(sortedKeys, newStorageValueArray) {
         newStorageValueArray.push(newValue);   
     })
 }
+
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+
 
 export { reOrderStorage, populateStorage, renderStorageOnPage, removeStorageItem, removeProjectStorageItem, useLocalStorageInputs }
